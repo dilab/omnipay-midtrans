512fe0f3b0832f4e60e318d2546f0cea9db670e3
xu ding <thedilab@gmail.com>
2017-12-19 12:25:15 +0800

    handle json format

--- src/Message/SnapWindowRedirectionCompletePurchaseRequest.php
+++ src/Message/SnapWindowRedirectionCompletePurchaseRequest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Omnipay\Midtrans\Message;
 
 
@@ -7,13 +8,18 @@ class SnapWindowRedirectionCompletePurchaseRequest extends AbstractRequest
 
     public function getData()
     {
+        if (0 === strpos($this->httpRequest->headers->get('Content-Type'), 'application/json')) {
+            $data = json_decode($this->httpRequest->getContent(), true);
+            $this->httpRequest->request->replace(is_array($data) ? $data : array());
+        }
+
         return $this->httpRequest->request->all();
     }
 
     public function sendData($data)
     {
         return new SnapWindowRedirectionCompletePurchaseResponse(
-            $this,$data
+            $this, $data
         );
     }
 
